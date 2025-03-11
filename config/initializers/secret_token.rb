aceaf56933e03cd9752f5bf3e549285f85a1b35b
Kyler Ries <547330+kylerries@users.noreply.github.com>
2025-03-10 22:51:11 -0700

    Delete config directory

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'b36dcefa58fabe2cd5e99b1f672b6f516d75e23c5250f7afb50d86be53f30b3bffc9a41e6ddaa4721afe268cb27805b75e46fb4e0d2e43fe533fbe10ad17cfa6'
